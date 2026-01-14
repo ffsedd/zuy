76083860c59f20d57a5562ec27c82c784c4064da
ffsedd <ffsedd@gmail.com>
2026-01-14 15:09:47 +0100

    minor

--- src/zuy/cli/spectrum_cli.py
+++ src/zuy/cli/spectrum_cli.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-# zuy/spectrum/cli.py
-=======
 #!/usr/bin/env python3
 from __future__ import annotations
 
 import argparse
->>>>>>> 9a9d1ef (spectrumcli)
 from pathlib import Path
 
 import matplotlib.pyplot as plt  # type: ignore
@@ -19,17 +15,6 @@ from zuy.spectrum.processing import tidy_spectrum
 from zuy.spectrum.squre_root_scale import register_sqrt_scale
 
 register_sqrt_scale()
-<<<<<<< HEAD
-
-logger = setup_logger(__name__)
-
-
-def main(data_dir: str | Path = ".") -> None:
-    data_dir = Path(data_dir)
-    fpaths = natsort.natsorted(data_dir.rglob("*.msa"), key=str)
-    spectra = [parse_msa_file(f) for f in fpaths]
-    tidy_spectra = [tidy_spectrum(s, smooth_window=5) for s in spectra]
-=======
 logger = setup_logger(__name__)
 
 
@@ -76,45 +61,27 @@ def main(data_dir: Path, fpath: Path | None, smooth_window: int) -> None:
         spectra = load_from_dir(data_dir)
 
     tidy_spectra = [tidy_spectrum(s, smooth_window=smooth_window) for s in spectra]
->>>>>>> 9a9d1ef (spectrumcli)
 
     plt.rc(
         "axes",
         prop_cycle=cycler("linestyle", ["-", "--", ":", "-."]) * plt.rcParams["axes.prop_cycle"],
     )
-<<<<<<< HEAD
-    plot_multiple_spectra(spectra)
-    ax = plt.gca()
-    ax.set_yscale("sqrt")
-    ax.set_xlim(left=0, right=14)
-    ax.set_ylim(bottom=-50)
-=======
 
     plot_multiple_spectra(spectra)
     ax = plt.gca()
     ax.set_yscale("sqrt")
     ax.set_xlim(0, 14)
     ax.set_ylim(-50)
->>>>>>> 9a9d1ef (spectrumcli)
 
     plot_multiple_spectra(tidy_spectra)
     ax = plt.gca()
     ax.set_yscale("sqrt")
-<<<<<<< HEAD
-    ax.set_xlim(left=0, right=14)
-    ax.set_ylim(bottom=-50)
-=======
     ax.set_xlim(0, 14)
     ax.set_ylim(-50)
 
->>>>>>> 9a9d1ef (spectrumcli)
     plt.show()
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    main(Path(__file__).parent.parent / "data")
-=======
     args = parse_args()
     main(args.data_dir, args.file, args.smooth_window)
->>>>>>> 9a9d1ef (spectrumcli)
